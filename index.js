be221e5360e880e96202805fd5d1586e18c43f26
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-11-23 20:12:01 +0700

    ok

--- index.js
+++ index.js
@@ -378,7 +378,7 @@ http.createServer(async function (req, res) {
         //writehead json
         res.writeHead(200, { 'Content-Type': 'application/json' });
         if(info === 'true'){
-            res.write(newway);
+            res.write(JSON.stringify([newway]));
         }else{
             res.write(JSON.stringify(data));
         }
