f421af2fa827cecc50898151c2635c1eca42dbac
Phongsakorn Wisetthon <3013842+boyphongsakorn@users.noreply.github.com>
2026-03-10 03:02:27 +0000

    Improve data assignment logic in price retrieval to enhance accuracy and debugging

--- index.js
+++ index.js
@@ -1907,7 +1907,15 @@ fastify.get('/', async (request, reply) => {
     if (data[1][10] == null && data[0].filter(x => !data[1].includes(x)).length <= 3) {
         data[0][0] = data[1][0];
         let newwow = await getData();
-        data[1] = newwow[1];
+        console.log("data[1][0] : " + data[1][0]);
+        console.log("newwow[0][0] : " + newwow[0][0]);
+        console.log(data);
+        console.log(newwow);
+        if (newwow[1][0] == data[0][0]) {
+            data[1] = newwow[0];
+        } else {
+            data[1] = newwow[1];
+        }
         if(data[1][10] == null || data[1][10] == undefined){
             if (parseFloat(data[1][7]) > parseFloat(data[0][7].toString())) {
                 data[1][10] = parseFloat(data[0][10]) + (parseFloat(data[1][7].toString()) - parseFloat(data[0][7].toString()));
